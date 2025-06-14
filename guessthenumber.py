11238459c3b473ae2f85e33db8914b53cdd3ef96
AarjanKarki <aarjankarki2000@gmail.com>
2025-06-14 17:43:11 +0100

    Add files via upload

--- guessthenumber.py
+++ guessthenumber.py
@@ -7,8 +7,10 @@ def showguess():
     window2.geometry('500x300')
 
     number=random.randint(0,20)
+    message=StringVar()
+    message.set(f"{name_var.get()} the number is {number}")
 
-    lab=Label(window2,width=20,textvariable=name'your number is'number)
+    lab=Label(window2,width=20,textvariable=message)
     lab.grid(row=1,column=1)
 
     window2.mainloop
@@ -17,17 +19,18 @@ if __name__=='__main__':
     window=Tk()
     window.geometry('500x300')
 
-    name=StringVar()
+    name_var=StringVar()
     guess_=StringVar()
 
     labelname=Label(window,text='Please enter your name')
     labelname.grid(row=1,column=1,padx=5,pady=5)
 
-    ntryname=Entry(window)
+    ntryname=Entry(window,textvariable=name_var)
     ntryname.grid(row=2,column=1,padx=5,pady=5)
 
     def ok():
-        name=ntryname.get()
+        name_var=ntryname.get()
+        print(name_var)
 
     btnok=Button(window,text='ok',command=ok)
     btnok.grid(row=2,column=2,padx=5,pady=10)
